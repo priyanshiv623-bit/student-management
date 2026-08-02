46fbc2804b4556c01b0e197a66c9bcda21c3ee85
priyanshiv623-bit <priyanshiv623@gmail.com>
2026-08-02 20:50:06 +0530

    Configure Django for Render

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3vx(!!yh)i7$ahgfgu5ngy_*2u=bie&(2^$$zczwto6x=2cwlb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
